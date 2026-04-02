385f5e34f1c2f6519ed1071b8d2d5da0e9eb9c63
wangjin <i@mkideal.com>
2026-04-02 23:26:11 +0800

    增加魔方教学文章

--- website/sidebars.ts
+++ website/sidebars.ts
@@ -32,7 +32,47 @@ const sidebars: SidebarsConfig = {
     {
       type: "category",
       label: "Rubik's Cube",
-      items: ["rubik/tutorials/index", "rubik/tutorials/2x2", "rubik/tutorials/pyraminx", "rubik/tutorials/skewb", "rubik/tutorials/4x4"],
+      items: [
+        {
+          type: "category",
+          label: "Beginner Tutorials",
+          items: [
+            "rubik/tutorials/index",
+            "rubik/tutorials/2x2",
+            "rubik/tutorials/pyraminx",
+            "rubik/tutorials/skewb",
+            "rubik/tutorials/4x4",
+          ],
+        },
+        {
+          type: "category",
+          label: "Advanced 3x3",
+          items: [
+            "rubik/tutorials/cfop",
+            "rubik/tutorials/f2l",
+            "rubik/tutorials/oll",
+            "rubik/tutorials/pll",
+          ],
+        },
+        {
+          type: "category",
+          label: "Technique & Speed",
+          items: [
+            "rubik/tutorials/finger-tricks",
+            "rubik/tutorials/speed-tips",
+          ],
+        },
+        {
+          type: "category",
+          label: "Advanced Puzzles",
+          items: [
+            "rubik/tutorials/2x2-advanced",
+            "rubik/tutorials/4x4-advanced",
+            "rubik/tutorials/pyraminx-advanced",
+            "rubik/tutorials/skewb-advanced",
+          ],
+        },
+      ],
     },
   ],
 };
